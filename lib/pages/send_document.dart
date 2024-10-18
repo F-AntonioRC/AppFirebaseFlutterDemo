c81ad8e3e77734182f90c1e36f3f1c16249a154f
F-AntonioRC <l20920144@voaxaca.tecnm.mx>
2024-10-18 12:36:43 -0600

    add document

---
+++ lib/pages/send_document.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SendDocument extends StatefulWidget {
+  const SendDocument({super.key});
+
+  @override
+  State<SendDocument> createState() => _SendDocumentState();
+}
+
+class _SendDocumentState extends State<SendDocument> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
