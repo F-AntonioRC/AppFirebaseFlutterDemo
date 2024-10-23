b0c5e20c094afd0ffb826721408a4f5b6fd53b97
F-AntonioRC <l20920144@voaxaca.tecnm.mx>
2024-10-22 20:16:14 -0600

    changes of UI - Dashbard UI changes

--- lib/graphics/data/pie_chart.dart
+++ lib/graphics/data/pie_chart.dart
@@ -21,72 +21,72 @@ class _CustomPieChartState extends State<CustomPieChart> {
       aspectRatio: 1.3,
       child: Card(
         color: ligth,
-        child: Column(
-          children: [
-            Text(widget.title, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-            const SizedBox(height: 18.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-
-                Indicator(
-                  color: Colors.blue,
-                  text: 'Uno',
-                  isSquare: false,
-                  size: touchedIndex == 0 ? 18 : 16,
-                  textColor: touchedIndex == 0 ? greenColor : darkBackground,
-                ),
-                Indicator(
-                  color: Colors.green,
-                  text: 'Dos',
-                  isSquare: false,
-                  size: touchedIndex == 1 ? 18 : 16,
-                  textColor: touchedIndex == 1 ? greenColor : darkBackground,
-                ),
-              ],
-            ),
-            const SizedBox(height: 18.0),
-            Expanded(
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: PieChart(
-                  PieChartData(
-                    pieTouchData: PieTouchData(
-                      touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                        setState(() {
-                          if (!event.isInterestedForInteractions ||
-                              pieTouchResponse == null ||
-                              pieTouchResponse.touchedSection == null) {
-                            touchedIndex = -1;
-                            return;
-                          }
-                          touchedIndex =
-                              pieTouchResponse.touchedSection!.touchedSectionIndex;
-                        });
-                      },
+        child: Padding(padding: const EdgeInsets.all(5.0),
+          child: Column(
+            children: [
+              Text(
+                widget.title,
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 18.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Indicator(
+                    color: Colors.blue,
+                    text: 'Uno',
+                    isSquare: false,
+                    size: touchedIndex == 0 ? 18 : 16,
+                    textColor: touchedIndex == 0 ? greenColor : darkBackground,
+                  ),
+                  Indicator(
+                    color: Colors.green,
+                    text: 'Dos',
+                    isSquare: false,
+                    size: touchedIndex == 1 ? 18 : 16,
+                    textColor: touchedIndex == 1 ? greenColor : darkBackground,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 18.0),
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: PieChart(
+                    PieChartData(
+                      pieTouchData: PieTouchData(
+                        touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                          setState(() {
+                            if (!event.isInterestedForInteractions ||
+                                pieTouchResponse == null ||
+                                pieTouchResponse.touchedSection == null) {
+                              touchedIndex = -1;
+                              return;
+                            }
+                            touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
+                          });
+                        },
+                      ),
+                      startDegreeOffset: 180,
+                      borderData: FlBorderData(show: false),
+                      sectionsSpace: 1,
+                      centerSpaceRadius: 0,
+                      sections: showingSections(), // Asegurarte de generar solo 2 secciones.
                     ),
-                    startDegreeOffset: 180,
-                    borderData: FlBorderData(show: false),
-                    sectionsSpace: 1,
-                    centerSpaceRadius: 0,
-                    sections: showingSections(),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),),
       ),
     );
   }
 
   List<PieChartSectionData> showingSections() {
-    return List.generate(4, (i) {
+    return List.generate(2, (i) { // Cambiar a 2 en lugar de 4
       final isTouched = i == touchedIndex;
       const color0 = Colors.blue;
       const color1 = Colors.green;
-      const color2 = Colors.yellow;
-      const color3 = Colors.red;
 
       switch (i) {
         case 0:
@@ -94,7 +94,7 @@ class _CustomPieChartState extends State<CustomPieChart> {
             color: color0,
             value: 75,
             title: '',
-            radius: 80,
+            radius: isTouched ? 80 : 65, // Ajustar radio si es tocado
             titlePositionPercentageOffset: 0.55,
             borderSide: isTouched
                 ? const BorderSide(color: Colors.white, width: 6)
@@ -105,7 +105,7 @@ class _CustomPieChartState extends State<CustomPieChart> {
             color: color1,
             value: 25,
             title: '',
-            radius: 65,
+            radius: isTouched ? 80 : 65, // Ajustar radio si es tocado
             titlePositionPercentageOffset: 0.55,
             borderSide: isTouched
                 ? const BorderSide(color: Colors.white, width: 6)
--- lib/pages/dashboard_main.dart
+++ lib/pages/dashboard_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:testwithfirebase/components/card_graphics.dart';
+import 'package:testwithfirebase/graphics/data/line_chart_sample2.dart';
 import 'package:testwithfirebase/graphics/data/pie_chart.dart';
 
 class DashboardMain extends StatelessWidget {
@@ -7,12 +8,24 @@ class DashboardMain extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-    children: [
-        Expanded( child: CardGraphics(cardWidget: CustomPieChart(title: 'Cursos completados',)),),
-      Expanded(child: CardGraphics(cardWidget: CustomPieChart(title: 'Curso uso de facebook',)),)
-    ],
-      );
-
+    return const SingleChildScrollView(
+      child:
+      Column(
+        children: [
+          Row(
+            children: [
+              Expanded( child: CardGraphics(cardWidget: CustomPieChart(title: 'Cursos completados',)),),
+              Expanded( child: CardGraphics(cardWidget: CustomPieChart(title: 'Cursos Añadidos',)),),
+              Expanded(child: CardGraphics(cardWidget: CustomPieChart(title: 'Curso uso de facebook',)),)
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(child: CardGraphics(cardWidget: LineChartSample2())),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
