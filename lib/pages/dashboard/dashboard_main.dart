18b9962dc7d5febddecb1bc5d289b54f245324bc
F-AntonioRC <l20920144@voaxaca.tecnm.mx>
2025-01-05 20:07:39 -0600

    Mejora en la UI Agregación de graficas dinamias con los datos de la BD

--- lib/pages/dashboard/dashboard_main.dart
+++ lib/pages/dashboard/dashboard_main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:testwithfirebase/graphics/pieChart/graphic_pie_chart.dart';
-import 'package:testwithfirebase/graphics/lineChart/graphic_line_chart.dart';
 import 'package:testwithfirebase/graphics/pieChart/main_pie_chart.dart';
 import 'package:testwithfirebase/pages/dashboard/screen_lines_graphics.dart';
-import '../../components/dashboard_components/card_graphics.dart';
-import '../../graphics/data/pie_chart.dart';
 
 class DashboardMain extends StatelessWidget {
   const DashboardMain({super.key});
