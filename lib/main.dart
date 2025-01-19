e4809b1339165b702f6e483d84c8f2a3c315ed7b
F-AntonioRC <l20920144@voaxaca.tecnm.mx>
2025-01-19 10:59:37 -0600

    Add new Environment Variables in the project from the connection with the database and a library for protection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:testwithfirebase/auth/auth_gate.dart';
 import 'package:testwithfirebase/firebase_options.dart';
@@ -8,6 +9,11 @@ import 'package:testwithfirebase/providers/edit_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    throw Exception('Error loading .env file: $e');
+  }
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
