7faf99e030916398bd712429a0f5185d96216ab8
F-AntonioRC <l20920144@voaxaca.tecnm.mx>
2025-02-15 18:37:13 -0600

    Se aplicaron cambios en la UI y mejoro el uso de actualizaciones con la base de datos

--- lib/pages/notification/notification_new.dart
+++ lib/pages/notification/notification_new.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:testwithfirebase/dataConst/constand.dart';
 import 'package:testwithfirebase/pages/notification/notification_service.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:url_launcher/url_launcher.dart';
